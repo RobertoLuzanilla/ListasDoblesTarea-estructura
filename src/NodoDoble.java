68a406fe0cd884447f9d2d14a7faacb34d235bc7
RobertoLuzanilla <luzanillasanchez@gmail.com>
2023-03-10 14:21:29 -0700

    He creado el primer metodo
    He creado el metodo insertar inicio

---
+++ src/NodoDoble.java
@@ -0,0 +1,11 @@
+public class NodoDoble {
+    int dato;
+
+    NodoDoble sig,ant;
+    NodoDoble (int d){
+        dato=d;
+        sig=null;
+        ant=null;
+    }
+
+}
--- src/Oper_ListasDobles.java
+++ src/Oper_ListasDobles.java
@@ -1,2 +1,16 @@
 public class Oper_ListasDobles {
+     NodoDoble prim=null, ulti=null;
+
+     public void insertarInicio(int x){
+         NodoDoble nuevo=new NodoDoble(x);
+
+         if(prim==null){
+             prim=nuevo;
+             ulti=nuevo;
+         }else {
+             nuevo.sig=prim;
+             prim.ant=nuevo;
+            prim=nuevo;
+         }
+     }
 }
