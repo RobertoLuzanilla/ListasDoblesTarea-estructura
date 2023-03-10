43c7ab924c331d7d2264504fd9ea3aaa5c8f725c
RobertoLuzanilla <luzanillasanchez@gmail.com>
2023-03-10 13:34:32 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int opcion=0, dato1, ubicacion;
+
+        do {
+
+            System.out.println("Menu de listas dobles");
+            System.out.println("1.-Insertar inicio" +
+                    "\n2.-Insertar final" +
+                    "\n3.-Insertar enseguida" +
+                    "\n4.-Eliminar al inicio" +
+                    "\n5.-Eliinar al final" +
+                    "\n6.-Eliminar elemento" +
+                    "\n7.-Mostrar" +
+                    "\n8.-Salir");
+                    opcion =sc.nextInt();
+
+                    switch (opcion) {
+                        case 1:
+                            System.out.println("Insertar inicio");
+                            System.out.println("Introduzca el digito que desea");
+                            dato1 = sc.nextInt();
+
+                            break;
+                        case 2:
+                            System.out.println("Insertar final");
+                            System.out.println("Introduzca el digito que desea");
+                            dato1 = sc.nextInt();
+                            break;
+                        case 3:
+                            System.out.println("Insertar enseguida");
+                            System.out.println("Introduzca el digito que desea insertar");
+                            dato1 = sc.nextInt();
+                            System.out.println("Introduzca el digito al que desea insertar enseguida");
+                            ubicacion = sc.nextInt();
+                            break;
+                        case 4:
+                            System.out.println("Eliminar al inicio");
+
+                            break;
+                        case 5:
+                            System.out.println("Eliminar al final");
+
+                            break;
+                        case 6:
+                            System.out.println("Eliminar elemento");
+                            System.out.println("Introduzca que eleento quiere eliminar");
+                            ubicacion = sc.nextInt();
+                            break;
+                        case 7:
+                            System.out.println("Mostrar");
+
+                            break;
+                        case 8:
+
+                            System.out.println("Gracias por esar el programa");
+                            break;
+                    }
+        }while (opcion!=8);
+    }
+}
